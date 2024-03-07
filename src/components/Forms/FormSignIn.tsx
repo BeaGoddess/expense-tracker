9b985c55479526d87383ab57bc275805a9903e95
Beatriz Silva <beagaara@gmail.com>
2024-03-07 16:58:17 +0000

    refactor: changed function about signin (#4)

--- src/components/Forms/FormSignIn.tsx
+++ src/components/Forms/FormSignIn.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import { signin } from "@/app/(auth)/sign-in/action";
 import Button from "@/components/Button/Button";
 import Input from "@/components/Input/Input";
-
+import { useState } from "react";
 
 export default function FormSignIn() {
+  const [loading, setLoading] = useState(false);
+
+  async function signInWithEmail(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    setLoading(true);
+    await signin(new FormData(event.currentTarget));
+    setLoading(false);
+  }
+
   return (
-    <form action={signin}>
+    <form onSubmit={signInWithEmail}>
       <Input type="email" name="email" placeholder="Email" variant="sign-in" />
 
       <Input
@@ -15,7 +27,12 @@ export default function FormSignIn() {
         variant="sign-in"
       />
 
-      <Button text="Login" variant="sign-in" type={"submit"} />
+      <Button
+        text={loading ? "Logging in" : "Login"}
+        variant="sign-in"
+        type={"submit"}
+        disabled={loading}
+      />
     </form>
   );
 }
