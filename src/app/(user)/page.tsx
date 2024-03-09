cd2c50f3e42af211b88e22f7079b51fb88342920
Beatriz Silva <beagaara@gmail.com>
2024-03-09 14:26:35 +0000

    refactor: changed home page content

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className={`h-screen text-black flex justify-center items-center text-[105px] bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 background-animate`}>
-      OLÁ, FOSTE HACKEADO
+      🐼 Still in development 🚧
     </div>
   );
 }
