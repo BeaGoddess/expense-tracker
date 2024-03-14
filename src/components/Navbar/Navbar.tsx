354da89bb449d1fdd111e954a764b624a41aabb3
Beatriz Silva <beagaara@gmail.com>
2024-03-14 15:29:41 +0000

    feat: replaced emojis for icons (#9)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,33 +1,40 @@
 import TitleBar from "./TitleNavbar";
+import {
+  HomeIcon,
+  WalletIcon,
+  BanknotesIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
 
 export default function NavBar() {
   return (
     <div className="w-[280px] text-white h-full bg-gradient-to-b from-[#8E8CDA] to-[#C0BAFF] p-6 ">
       <TitleBar />
-      <div className="my-4 font-medium">
-        <div className="flex p-2 gap-4">
-          <div> 🏠 </div>
+      <div className="my-4 font-medium text-lg text-white">
+        <div className="flex p-2 gap-6 items-center">
+          <HomeIcon className="h-5 w-5" />
           <div> Home </div>
         </div>
       </div>
 
-      <div className="my-4 font-bold bg-white text-[#7574C7] rounded-md">
-        <div className="flex p-2 gap-4">
-          <div> 💳 </div>
+      <div className="my-4 font-bold bg-white text-[#7574C7] rounded-md text-lg">
+        <div className="flex p-2 gap-6 items-center">
+          <WalletIcon className="h-5 w-5" />
+
           <div> Wallets </div>
         </div>
       </div>
 
-      <div className="my-4 font-medium">
-        <div className="flex p-2 gap-4">
-          <div> 💰 </div>
+      <div className="my-4 font-medium text-lg">
+        <div className="flex p-2 gap-6 items-center">
+          <BanknotesIcon className="h-5 w-5" />
           <div> Transactions </div>
         </div>
       </div>
 
-      <div className="my-4 font-medium">
-        <div className="flex p-2 gap-4">
-          <div> 💃🏼 </div>
+      <div className="my-4 font-medium text-lg">
+        <div className="flex p-2 gap-6 items-center">
+          <UserIcon className="h-5 w-5" />
           <div> Profile </div>
         </div>
       </div>
--- src/components/Navbar/TitleNavbar.tsx
+++ src/components/Navbar/TitleNavbar.tsx
@@ -1,12 +1,14 @@
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
 export default function TitleBar() {
   return (
-    <div className="flex mt-8 mb-14">
+    <div className="flex mt-8 mb-12">
       <div className="font-bold text-4xl leading-tight text-white">
         <span className="text-[#7574C7]">E</span>xpense{" "}
         <span className="text-[#7574C7]">T</span>racker
       </div>
 
-      <div className="pt-2 text-3xl text-white font-bold text-center"> = </div>
+      <Bars3Icon className="h-8 w-8 mt-2 text-white stroke-[3px]" />
     </div>
   );
 }
