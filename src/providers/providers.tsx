c70700ffac9f4d7406853297a64cf4904f1cba05
Beatriz Silva <beagaara@gmail.com>
2024-03-26 17:15:13 +0000

    fix: changed breakpoints theme

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,7 +1,18 @@
-'use client'
+"use client";
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const breakpoints = {
+  base: "0px",
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px",
+};
+
+const theme = extendTheme({ breakpoints });
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
