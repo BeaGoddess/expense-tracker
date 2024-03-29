5053c00cc9b1b8dc2a9af99562146e33fa6fb55b
Beatriz Silva <beagaara@gmail.com>
2024-03-29 21:37:28 +0000

    feat: created expenses hook to get data from supabase (#20)

--- src/hooks/useExpenses.ts
+++ src/hooks/useExpenses.ts
@@ -2,28 +2,28 @@ import useSupabaseBrowser from "@/utils/supabase/client";
 import { InsertTables, Tables } from "@/types/type";
 import { useState } from "react";
 
-export const useWallets = () => {
+export const useExpenses = () => {
   const client = useSupabaseBrowser();
-  const [wallets, setWallets] = useState<Tables<"wallets">[]>([]);
+  const [expenses, setExpenses] = useState<Tables<"expenses">[]>([]);
 
-  const getWallets = async () => {
+  const getExpenses = async () => {
     const {
       data: { user },
     } = await client.auth.getUser();
 
     if (user) {
       const { data, error } = await client
-        .from("wallets")
+        .from("expenses")
         .select("*")
         .eq("user_id", user?.id);
 
       if (data) {
-        setWallets(data);
+        setExpenses(data);
       }
     }
   };
 
-  const createWallet = async (row: InsertTables<"wallets">) => {
+  const createExpense = async (row: InsertTables<"expenses">) => {
     const {
       data: { user },
     } = await client.auth.getUser();
@@ -31,16 +31,16 @@ export const useWallets = () => {
     if (user) {
       const userId = user?.id;
 
-      const { data: newWallet, error } = await client
-        .from("wallets")
+      const { data: newExpense, error } = await client
+        .from("expenses")
         .insert({ user_id: userId, ...row })
         .select()
         .single();
 
-      if (newWallet) {
-        setWallets((prev) => [...prev, newWallet]);
+      if (newExpense) {
+        setExpenses((prev) => [...prev, newExpense]);
 
-        return { success: true, wallet: newWallet };
+        return { success: true, expense: newExpense };
       }
 
       if (error) {
@@ -49,7 +49,7 @@ export const useWallets = () => {
     }
   };
 
-  const deleteWallet = async (walletId: number) => {
+  const deleteExpense = async (expenseId: number) => {
     const {
       data: { user },
     } = await client.auth.getUser();
@@ -58,27 +58,25 @@ export const useWallets = () => {
       const userId = user?.id;
 
       const { error } = await client
-        .from("wallets")
+        .from("expenses")
         .delete()
-        .eq("id", walletId)
+        .eq("id", expenseId)
         .eq("user_id", userId);
 
       if (error) {
         return { error: error.message };
       }
 
-      setWallets((prevWallets) =>
-        prevWallets.filter((wallet) => wallet.id !== walletId)
-      );
+      setExpenses((prev) => prev.filter((expense) => expense.id !== expenseId));
 
       return { success: true };
     }
   };
 
   return {
-    wallets,
-    getWallets,
-    createWallet,
-    deleteWallet,
+    expenses,
+    getExpenses,
+    createExpense,
+    deleteExpense,
   };
 };
